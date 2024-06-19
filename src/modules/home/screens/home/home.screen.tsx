1bac0d46ffce46b4d89f0afe4ec91522999341b2
PavloPushak <pashapushak1996@gmail.com>
2024-06-19 12:15:39 +0300

    feat: connect user to the chat

--- src/modules/home/screens/home/home.screen.tsx
+++ src/modules/home/screens/home/home.screen.tsx
@@ -1,12 +1,57 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { StreamChat } from 'stream-chat';
 
-import { Text, View } from 'react-native';
+import { STREAM_CHAT_API_KEY } from '@env';
 import { Skeleton } from 'src/shared/components/skeleton';
 
-import HeartIcon from 'src/assets/icons/heart.svg';
-import { COLORS } from 'src/shared/themes';
+export const chatClient = StreamChat.getInstance(STREAM_CHAT_API_KEY);
 
 export const HomeScreen = () => {
+	// We use it for loading the user
+	const [userLoading, setUserLoading] = useState(false);
+
+	useEffect(() => {
+		const initUser = async () => {
+			setUserLoading(true);
+			try {
+				// Here we connect the user
+				// TODO add real token instead of dev
+
+				await chatClient.connectUser(
+					{
+						id: 'jlahey',
+						name: 'Jim Lahey',
+						image: 'https://i.imgur.com/fR9Jz14.png',
+					},
+					chatClient.devToken('jlahey'),
+				);
+
+				console.log(chatClient.userID);
+			} catch (e) {
+				setUserLoading(false);
+				console.log(e);
+			}
+
+			setUserLoading(false);
+		};
+
+		initUser();
+
+		return () => {
+			chatClient.disconnectUser();
+		};
+	}, []);
+
+	if (userLoading) {
+		return (
+			<Skeleton>
+				<ActivityIndicator size="large" />
+			</Skeleton>
+		);
+	}
+
 	return (
 		<Skeleton>
 			<View>
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,4 +3,5 @@ declare module '@env' {
 	export const BASE_URL: string;
 	export const SUPABASE_URL: string;
 	export const SUPABASE_ANON_KEY: string;
+	export const STREAM_CHAT_API_KEY: string;
 }
