fad6d85cbc4a52fd444e48e6129af9909ccd4dd4
PavloPushak <pashapushak1996@gmail.com>
2024-06-19 12:05:08 +0300

    feat: adjusted stream-chat

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { RootNavigator } from './modules/navigation/components/root-navigator';
 import { useDevtools } from './shared/hooks/devtools.hook';
+import { OverlayProvider } from 'stream-chat-react-native';
+import { StyleSheet } from 'react-native';
 
 const App = () => {
 	useDevtools();
 
 	return (
-		<SafeAreaProvider>
-			<RootNavigator />
-		</SafeAreaProvider>
+		<GestureHandlerRootView style={styles.flex}>
+			<SafeAreaProvider>
+				<OverlayProvider>
+					<RootNavigator />
+				</OverlayProvider>
+			</SafeAreaProvider>
+		</GestureHandlerRootView>
 	);
 };
+
+const styles = StyleSheet.create({ flex: { flex: 1 } });
 
 export default App;
